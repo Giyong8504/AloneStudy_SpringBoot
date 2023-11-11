d4ce075a927d3a8dab850c709c78740833124504
Giyong8504 <kky5163@naver.com>
2023-11-11 18:58:25 +0900

    초기 설정 업로드

---
+++ src/main/java/me/giyong8504/SpringbootDeveloperApplication.java
@@ -0,0 +1,13 @@
+package me.giyong8504;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeveloperApplication.class, args);
+	}
+
+}
