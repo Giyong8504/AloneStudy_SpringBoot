1afee341a07751795d40f2a741944e8a04b0d63e
Kim Giyong <128211712+Giyong8504@users.noreply.github.com>
2023-11-13 16:48:08 +0900

    초기디렉터리 구성, 요청 및 응답 과정 테스트

---
+++ src/main/java/me/giyong8504/MemberRepository.java
@@ -0,0 +1,8 @@
+package me.giyong8504;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
--- src/test/java/me/giyong8504/SpringBootDeveloperApplicationTests.java
+++ src/test/java/me/giyong8504/SpringBootDeveloperApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootDeveloperApplicationTests {
+class SpringBootDeveloperApplicationTests {
 
 	@Test
 	void contextLoads() {
